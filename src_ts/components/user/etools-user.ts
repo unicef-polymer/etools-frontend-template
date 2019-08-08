2c6c7788c73bcf2a967b63fa0f7dd8778d40f49b
Adrian Hangan <iovuadrian@gmail.com>
2019-08-08 14:30:56 +0300

    added countries dropdown and profile dropdown (#24)
    countries dropdown and profile dropdown functionality

--- src_ts/components/user/etools-user.ts
+++ src_ts/components/user/etools-user.ts
@@ -6,8 +6,10 @@ import {connect} from 'pwa-helpers/connect-mixin';
 import {RootState, store} from '../../redux/store';
 import {getEndpoint} from '../../endpoints/endpoints';
 import {GenericObject} from '../../types/globals';
+import {updateUserData} from '../../redux/actions/user';
 
 const PROFILE_ENDPOINT = 'userProfile';
+const CHANGE_COUNTRY_ENDPOINT = 'changeCountry';
 
 /**
  * @customElement
@@ -20,17 +22,44 @@ export class EtoolsUser extends connect(store)(EtoolsAjaxRequestMixin(PolymerEle
   userData: EtoolsUserModel | null = null;
 
   private profileEndpoint = getEndpoint(PROFILE_ENDPOINT);
+  private changeCountryEndpoint = getEndpoint(CHANGE_COUNTRY_ENDPOINT);
 
   public stateChanged(state: RootState) {
     this.userData = state.user!.data;
-    console.log('[EtoolsUser]: store user data', state.user!.data);
+    console.log('[EtoolsUser]: store user data received', state.user!.data);
   }
 
   public getUserData() {
-    this.sendRequest({endpoint: this.profileEndpoint}).then((response: GenericObject) => {
-      console.log(response);
+    return this.sendRequest({endpoint: this.profileEndpoint}).then((response: GenericObject) => {
+      // console.log('response', response);
+      store.dispatch(updateUserData(response));
     }).catch((error: GenericObject) => {
-      console.log(error);
+      console.error('[EtoolsUser]: getUserData req error...', error);
+      throw error;
+    });
+  }
+
+  public updateUserData(profile: GenericObject) {
+    return this.sendRequest({
+      method: 'PATCH',
+      endpoint: this.profileEndpoint,
+      body: profile
+    }).then((response: GenericObject) => {
+      store.dispatch(updateUserData(response));
+    }).catch((error: GenericObject) => {
+      console.error('[EtoolsUser]: updateUserData req error ', error);
+      throw error;
+    });
+  }
+
+  public changeCountry(countryId: number) {
+    return this.sendRequest({
+      method: 'POST',
+      endpoint: this.changeCountryEndpoint,
+      body: {country: countryId}
+    }).catch((error: GenericObject) => {
+      console.error('[EtoolsUser]: updateUserData req error ', error);
+      throw error;
     });
   }
 
