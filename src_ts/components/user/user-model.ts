83cbb5d37255683ed49ac945a13b2ee2da8dc55e
lajoskovago <kovago.lajos91@gmail.com>
2020-04-16 09:04:00 +0300

    added dummy data for profile dialog

--- src_ts/components/user/user-model.ts
+++ src_ts/components/user/user-model.ts
@@ -22,3 +22,60 @@ export interface EtoolsUserModel {
   vendor_number: string | null;
   [key: string]: any;
 }
+
+export const dummyUserData = {
+  countries_available: [
+    {
+      'business_area_code': '4590',
+      'id': 46,
+      'name': 'Burkina Faso'
+    },
+    {
+      'business_area_code': '0060',
+      'id': 24,
+      'name': 'Afghanistan'
+    },
+    {
+      'business_area_code': '2130',
+      'id': 13,
+      'name': 'Iraq'
+    },
+    {
+      'business_area_code': '2490',
+      'id': 9,
+      'name': 'Lebanon'
+    }
+  ],
+  groups: [
+    {
+      'id': 6,
+      'name': 'UNICEF User',
+      'permissions': [60, 324, 287, 288, 289]
+    }
+  ],
+  country: {
+    'id': 9,
+    'initial_zoom': 8,
+    'latitude': '33.85000',
+    'local_currency': 'US Dollar',
+    'longitude': '35.86000',
+    'name': 'Lebanon'
+  },
+  country_override: 9,
+  email: 'jane@demo.com',
+  first_name: 'Jane',
+  guid: '',
+  is_active: 'True',
+  is_staff: 'True',
+  is_superuser: 'True',
+  job_title: '',
+  last_login: '2020-04-01 09:55:21.303242+00:00',
+  last_name: 'Doe',
+  middle_name: '',
+  name: '',
+  office: '',
+  oic: 'oic',
+  user: 255577,
+  username: 'jane@unicef.org',
+  vendor_number: ''
+};
